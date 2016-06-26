e4295c873b220ecf3206cb892b2af3deb9329f1a
Gg <trashboard.gg.sam@gmail.com>
2016-06-26 22:28:41 +0200

    added new role (role.Defender)

---
+++ role.defender.js
@@ -0,0 +1,14 @@
+/**
+ * Defender role mainly used for Defending a room. 
+ * Should work togheter with healers.
+ */
+var roleDefender = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+    
+    }
+	 
+};
+
+module.exports = roleDefender;
